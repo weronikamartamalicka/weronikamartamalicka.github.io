27a6936f216631addce853717c99c9c7761ed4fa
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-07-26 18:57:32 +0200

    Corrected path for task controller

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://kodilla-tasks-wm.herokuapp.com/v1/tasks/';
+  const apiRoot = 'https://kodilla-tasks-wm.herokuapp.com/v1/';
   const trelloApiRoot = 'https://kodilla-tasks-wm.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
